0f38d018b2ffa0476d7e345a8afd7cc00f282665
Gauri Jain <gauri.g.jain@gmail.com>
2023-05-04 18:44:24 -0400

    fix icon

--- components/hero.tsx
+++ components/hero.tsx
@@ -36,7 +36,7 @@ export default function Hero() {
             <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center">
               {/* <div data-aos="fade-up" data-aos-delay="400">
                 <a className="btn text-white bg-purple-600 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0" href="#0">Start free trial</a>
-              </div>
+              </div> random thing :)
               <div data-aos="fade-up" data-aos-delay="600">
                 <a className="btn text-white bg-gray-700 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="#0">Learn more</a>
               </div> */}

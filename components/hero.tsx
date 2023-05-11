31770d9e43782bccf13ede363321a69e99b124dd
Mohit Karnani <mohitkarnani@gmail.com>
2023-05-10 21:55:04 -0400

    capitalize

--- components/hero.tsx
+++ components/hero.tsx
@@ -31,7 +31,7 @@ export default function Hero() {
 
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
-            <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out"><span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-purple-400">Coarct</span></h1>
+            <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out"><span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-purple-400">COARCT</span></h1>
             <p className="text-xl text-gray-400 mb-8" data-aos="fade-up" data-aos-delay="200">Decentralized, Anonymous Trials.</p>
             <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center">
               {/* <div data-aos="fade-up" data-aos-delay="400">
